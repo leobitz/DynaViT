fb97b1f1839c25893bfe2776f47d2bbbbae753b3
leobitz <amanyamy@gmail.com>
2022-12-18 05:14:58 +0000

    working param

--- train.sh
+++ train.sh
@@ -1,11 +1,11 @@
 
 python dyna-train.py --model deit_small_patch16_LS  \
 --data-path data/tinyimagenet/tiny-imagenet-200 \
---batch-size 64 --lr 0.00001  --weight-decay 0.05 \
+--batch-size 64 --lr 0.0001  --weight-decay 0.05 \
 --sched cosine --input-size 224 --eval-crop-ratio 1.0 \
 --reprob 0.0  --smoothing 0.0 --warmup-epochs 0 --drop 0.0  \
 --seed 0 --opt adamw --warmup-lr 1e-6 --mixup .8 --drop-path 0.05 \
 --cutmix 1.0 --unscale-lr  --color-jitter 0.3 --ThreeAugment \
---proj_name tinyimagenet --exp_name initial --num_workers 8 \
+--proj_name tinyimagenet --exp_name initial-norl --num_workers 8 \
 --max_epochs 128 --gpus 4 --grad-acc 1  --data-set TINYIMNET \
---rl-lr 0.0001 --rl-dropout 0.0 --rl-gamma 0.9 --dynamic
+--rl-lr 0.001 --rl-dropout 0.2 --rl-gamma 0.9 --dynamic
