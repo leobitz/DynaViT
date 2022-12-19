a159abac9020032c15470ee4f900feb536640489
Leo Bitz <amanyamy@gmail.com>
2022-12-18 19:21:02 -0800

    bug fix and mean std

--- models_v2.py
+++ models_v2.py
@@ -429,7 +429,7 @@ class dyna_vit_models(nn.Module):
 def deit_tiny_patch16_LS(pretrained=False, img_size=224, pretrained_21k = False, dynamic=False, rl_dropout=0.2,  **kwargs):
     model_class = dyna_vit_models if dynamic else vit_models
     model = model_class(
-        img_size = img_size, patch_size=4, embed_dim=384, depth=12, num_heads=3, mlp_ratio=1, qkv_bias=True,
+        img_size = img_size, patch_size=4, embed_dim=384, depth=7, num_heads=12, mlp_ratio=1, qkv_bias=True,
         norm_layer=partial(nn.LayerNorm, eps=1e-6),block_layers=Layer_scale_init_Block, rl_dropout=rl_dropout, **kwargs)
     
     return model
